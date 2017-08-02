b37ef18a5536d5c6d22ace1d4ae049c53afe126c
Denisse Villamar Herrera <de2791vi@gmail.com>
2017-08-01 22:28:21 -0500

    Relative y Table Layout

---
+++ M2Arq/Ej/TutorCafeRLayout/app/src/main/java/nextu/dvillamar/tutorcaferlayout/solnextu.java
@@ -0,0 +1,13 @@
+package nextu.dvillamar.tutorcaferlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class solnextu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_solnextu);
+    }
+}
